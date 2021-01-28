c61b7be56dd4ebf54edc3dadc13b0dc4925ea682
Raul Del Rio <inforeservas@casarufino.es>
2021-01-28 15:32:29 +0100

    Ejercicio ocimax terminado

---
+++ CarpetaRaul/Ejercicio_ocimax/ocimax.js
@@ -0,0 +1,12 @@
+/**
+ * Ejercicio entrada al cine OCIMAX
+ */
+let autorizacion = prompt('Ocimax no permite la entrada a personas que no esten vacunadas ¿Está usted vacunado?')
+
+if(autorizacion === 'si') {
+    console.log('Puede usted pasar')
+}else if (autorizacion === 'no') {
+    console.log('Lo sentimos no podemos dejarle sin la vacuna puesta')
+}else{
+    console.log(`disculpe no le oido bien, SI o NO`)
+}
