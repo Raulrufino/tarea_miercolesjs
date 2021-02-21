b2056df2d39fdb486a6301cd367e77c829fd9e89
Raul Del Rio <inforeservas@casarufino.es>
2021-02-21 21:57:32 +0100

    ejercicios terminados

--- ejercicioParadigma/js/cambioDivisa.js
+++ ejercicioParadigma/js/cambioDivisa.js
@@ -4,6 +4,11 @@ let euro = document.getElementById('valor01'),
     divisaExtranjera = 0.82,
     btn_cambio = document.getElementById('cambio'),
     verResultado = document.getElementById('resultado')
+    
+let dolar = document.getElementById('valor02'),
+        divisaExtranjera01 = 1.21,
+        btn_cambio02 = document.getElementById('cambio02'),
+        verResultado02 = document.getElementById('resultado02')
 
 
 // creamos la funcion para el cambio
@@ -12,6 +17,15 @@ btn_cambio.addEventListener('click', () => {
     let total = Number(euro.value) * Number(divisaExtranjera)
     verResultado.innerHTML = total 
 })
+
+
+// creamos la funcion para el cambio
+
+btn_cambio02.addEventListener('click', () => {
+    let total = Number(dolar.value) * Number(divisaExtranjera01)
+    verResultado02.innerHTML = total 
+})
+
 
 
 
